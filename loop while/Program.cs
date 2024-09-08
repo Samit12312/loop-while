92cbbbccde76e282f885ffea7ac341c43b30724a
Samit12312 <154971416+Samit12312@users.noreply.github.com>
2024-09-08 09:42:25 +0300

    Update Program.cs

--- loop while/Program.cs
+++ loop while/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,36 @@ namespace loop_while
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("insert 2 number a and b ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int count = 0;
+            
+            while (a>b)
+            {
+                a = a - b;
+                count++;
+               
+                
+            }
+                Console.WriteLine($"sharit {a}");
+            Console.WriteLine($"count {count}");
+
+            Console.ReadLine();
+        }
+        static void targil3()
+        {
+            Console.WriteLine("insert positive number");
+            int num = int.Parse(Console.ReadLine());
+            int n = 2;
+            while (num >= n)
+            {
+                n = n * 2;
+                if (n < num)
+                {
+                    Console.WriteLine(n);
+                }
+            }
         }
     }
 }
